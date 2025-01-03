c70180003ea6fddbe4b8ebf9079cc93eaeb8b16d
Geetahasani <geetahasani201@gmail.com>
2025-01-03 20:01:07 +0530

    Updated Spring Boot version to 3.2.3 for stability

--- src/Components/api.jsx
+++ src/Components/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8081/api/qna/ask";
+const API_URL = "http://localhost:8086/api/qna/ask";
 
 export const fetchChatResponse = async (question) => {
     try {
